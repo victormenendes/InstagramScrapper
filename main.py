d4ad23a2d13774b6ff69fa6e696930e9456a05aa
Victor Menendes <victormenendes3101@gmail.com>
2023-04-05 17:41:20 -0300

    Second Commit

--- main.py
+++ main.py
@@ -1,42 +1,17 @@
-import requests, json, random
-from pprint import pprint
-usernames = ["Anitta", "Xurrasco"]
-proxy = "http://username:password@PROXY_SERVER:PORT"
-output = {}
-def get_headers(username):
-    headers = {
-        "authority": "www.instagram.com",
-        "method": "GET",
-        "path": "/{0}/".format(username),
-        "scheme": "https",
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "en-GB, en-US;q=0.9,en;q=0.8",
-        "upgrade-insecure-requests": "1",
-        "Connection": "close",
-        "user-agent" : random.choice([
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36",
-            "Mozilla/5.0 (Windows NT 10.0; Win64; X64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36",
-            "Mozilla/5.0 (Windows NT 6.1; Win64; X64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
-            "Mozilla/5.0 (Windows NT 10.0; Win64; X64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
-            "Mozilla/5.0 (Windows NT 10.0; Win64; X64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
-            "Mozilla/5.0 (Windows NT 6.1; Win64; X64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
-        ])
-    }
-    return headers
-def main():
-    for username in usernames:
-        url = f"https://www.instagram.com/{username}/?__a=1&__d=dis"
-        response = requests.get(url, headers=get_headers(username), proxies = {'http': proxy, 'https': proxy})
-        if response.status_code == 200:
-            try:
-                resp_json = json.loads(response.text)
-            except:
-                print ("NAO FUNCIONOU CARALHO")
-                continue
-            else:
-                user_data = resp_json[]
-if __name__ == '__main__':
-    main()
-    pprint(output)
+import instaloader
+import os
+import sys
+
+# Cria uma instância do Instaloader
+L = instaloader.Instaloader()
+
+# Pega o nome de usuário a partir dos argumentos
+username = sys.argv[1]
+
+# Cria um diretório para salvar as fotos
+if not os.path.exists(username):
+    os.mkdir(username)
+
+# Faz o download das fotos do perfil
+for post in instaloader.Profile.from_username(L.context, username).get_posts():
+    L.download_post(post, target=f"{username}")
